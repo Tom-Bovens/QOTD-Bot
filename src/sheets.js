7bbbd7bba5ecd19870d94bf6a8fd70a2c05dc30c
Tom-Bovens <tombovens2003@gmail.com>
2020-08-06 15:49:55 +0200

    Missed a thing.

---
+++ src/sheets.js
@@ -0,0 +1,8 @@
+
+
+const findQuote = () => {
+    return '"Give a man a fish and you feed him for a day. Teach him to fish and you feed him for a lifetime." -Lao-Tzu';
+};
+
+exports.findQuote = findQuote;
+
