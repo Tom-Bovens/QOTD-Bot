a50df45da7857e358c944fd99cad99df270fb022
Tom-Bovens <Tombovens2003@gmail.com>
2020-09-26 12:47:28 +0200

    Linted code

--- src/index.js
+++ src/index.js
@@ -1,73 +1,31 @@
-
-
 const path = require('path');
+
 const envfile = `${process.cwd()}${path.sep}.env`;
 require('dotenv').config({
     path: envfile
 });
-const express = require('express')
+// const express = require('express');
 const ChipChat = require('chipchat');
-const log = require('debug')('tourguide')
+const log = require('debug')('tourguide');
 const get = require('lodash/get');
-const getQuote = require('./sheets')
+const getQuote = require('./sheets');
+
 const incrementor = {
     // This function adds delay to the message to ensure the messages are posted in the right order
     autoDelayTime: 500,
-    increment: function (timeToIncrease = 1) {
+    increment(timeToIncrease = 1) {
         this.autoDelayTime += (timeToIncrease * 1000);
         return this.autoDelayTime;
     },
-    set: function (timeToSet = 1) {
+    set(timeToSet = 1) {
         this.autoDelayTime = (timeToSet * 1000);
         return this.autoDelayTime;
     }
 };
-log(process.env.HOST,process.env.TOKEN)
+log(process.env.HOST, process.env.TOKEN);
 const errorCatch = (error) => {
     log(error);
-}
-
-const depositQuote = async (userId) => {
-    try {
-        let quote = await getQuote()
-        let conversation
-        const oldConvFinder = await bot.conversations.list({ name:'Daily Quotes', participants:{ user: userId } })
-        if (oldConvFinder.length > 0) {
-            conversation = oldConvFinder[0]
-            await bot.send(conversation.id, [
-                {
-                    text: quote,
-                    isBackchannel: false,
-                    role: 'bot',
-                    delay: incrementor.set(3)
-                }
-            ])
-        } else {
-            conversation = await bot.conversations.create(
-                { name: 'Daily Quotes', messages: [{ text: `Hey there. I couldn't find a conversation to drop your quote in. So i made a new one. Here's your quote.` }] }
-            )
-            await bot.send(conversation.id, [
-                {
-                    type: 'command',
-                    text: '/assign',
-                    meta: {
-                        "users": [
-                            userId
-                        ]
-                    }
-                },
-            ])
-            await bot.send(conversation.id, [
-                {
-                    text: quote,
-                    isBackchannel: false,
-                    role: 'bot',
-                    delay: incrementor.set(3)
-                }
-            ])
-        }
-    } catch (e) { errorCatch(e) }
-}
+};
 
 // Create a new bot instance
 const bot = new ChipChat({
@@ -76,9 +34,51 @@ const bot = new ChipChat({
     token: process.env.TOKEN
 });
 
+const depositQuote = async (userId) => {
+    try {
+        const quote = await getQuote();
+        let conversation;
+        const oldConvFinder = await bot.conversations.list({ name: 'Daily Quotes', participants: { user: userId } });
+        if (oldConvFinder.length > 0) {
+            conversation = oldConvFinder[0];
+            await bot.send(conversation.id, [
+                {
+                    text: quote,
+                    isBackchannel: false,
+                    role: 'bot',
+                    delay: incrementor.set(3)
+                }
+            ]);
+        } else {
+            conversation = await bot.conversations.create(
+                { name: 'Daily Quotes', messages: [{ text: 'Hey there. I couldn\'t find a conversation to drop your quote in. So i made a new one. Here\'s your quote.' }] }
+            );
+            await bot.send(conversation.id, [
+                {
+                    type: 'command',
+                    text: '/assign',
+                    meta: {
+                        users: [
+                            userId
+                        ]
+                    }
+                }
+            ]);
+            await bot.send(conversation.id, [
+                {
+                    text: quote,
+                    isBackchannel: false,
+                    role: 'bot',
+                    delay: incrementor.set(3)
+                }
+            ]);
+        }
+    } catch (e) { errorCatch(e); }
+};
+
 // Crashes the code if no token is found
 if (!process.env.TOKEN) {
-    throw 'No token found, please define a token with export TOKEN=(Webhook token), or use an .env file.'
+    throw new Error('No token found, please define a token with export TOKEN=(Webhook token), or use an .env file.');
 }
 
 // Use any REST resource
@@ -91,29 +91,29 @@ bot.on('error', log);
 
 bot.on('user.login', async (loginUser) => {
     try {
-        const user = await bot.users.get(get(loginUser, 'data.user.id'))
-        const userId = user.id
-        const hasSubscribed = get(user, 'meta.subscribedToQuotes', 'false')
-        log(`User status: ${hasSubscribed}`)
-        if (hasSubscribed === "false") {
-            log("User has not seen the guide on result comments yet.")
-            if (user.role == 'agent') {
+        const user = await bot.users.get(get(loginUser, 'data.user.id'));
+        const userId = user.id;
+        const hasSubscribed = get(user, 'meta.subscribedToQuotes', 'false');
+        log(`User status: ${hasSubscribed}`);
+        if (hasSubscribed === 'false') {
+            log('User has not seen the guide on result comments yet.');
+            if (user.role === 'agent') {
                 if (userId) {
-                    let conversation
-                    const oldConvFinder = await bot.conversations.list({ name:'Daily Quotes', participants:{ user: userId } })
+                    let conversation;
+                    const oldConvFinder = await bot.conversations.list({ name: 'Daily Quotes', participants: { user: userId } });
                     if (oldConvFinder.length > 0) {
-                        conversation = oldConvFinder[0]
+                        conversation = oldConvFinder[0];
                     } else {
                         conversation = await bot.conversations.create(
-                            { name: 'Daily Quotes', messages: [{ text: `Hey there. Zen Chip here.` }] }
-                        )
+                            { name: 'Daily Quotes', messages: [{ text: 'Hey there. Zen Chip here.' }] }
+                        );
                     }
                     await bot.send(conversation.id, [
                         {
                             type: 'command',
                             text: '/assign',
                             meta: {
-                                "users": [
+                                users: [
                                     userId
                                 ]
                             }
@@ -139,161 +139,160 @@ bot.on('user.login', async (loginUser) => {
                             delay: incrementor.increment(4),
                             actions: [
                                 {
-                                    type: "reply",
-                                    text: "Yeah, surprise me daily.",
-                                    payload: "userAccepted"
+                                    type: 'reply',
+                                    text: 'Yeah, surprise me daily.',
+                                    payload: 'userAccepted'
                                 },
                                 {
-                                    type: "reply",
-                                    text: "No thanks.",
-                                    payload: "userDenied"
+                                    type: 'reply',
+                                    text: 'No thanks.',
+                                    payload: 'userDenied'
                                 }
                             ]
                         }
-                    ])
+                    ]);
                 }
             }
-        } else if (hasSubscribed === "true") {
-            await depositQuote(userId)
+        } else if (hasSubscribed === 'true') {
+            await depositQuote(userId);
         }
-    } catch (e){
-        errorCatch(e)
+    } catch (e) {
+        errorCatch(e);
     }
 });
 
 bot.on('message.create.*.postback.*', async (message, conversation) => {
     try {
-        const userId = message.user
-        if (message.text == "userAccepted") {
+        const userId = message.user;
+        if (message.text === 'userAccepted') {
             await bot.send(conversation.id, [
                 {
-                    text: `Great, i'll be back with a new quote tomorrow.`,
+                    text: 'Great, i\'ll be back with a new quote tomorrow.',
                     isBackchannel: false,
                     role: 'bot',
                     delay: incrementor.set(3)
                 }
-            ])
-            await bot.users.update(userId, { meta: { subscribedToQuotes: 'true' }})
-        } else if (message.text == "userDenied") {
+            ]);
+            await bot.users.update(userId, { meta: { subscribedToQuotes: 'true' } });
+        } else if (message.text === 'userDenied') {
             await bot.send(conversation.id, [
                 {
-                    text: `Ok, i won't bother you again. If you change your mind then call me.`,
+                    text: 'Ok, i won\'t bother you again. If you change your mind then call me.',
                     isBackchannel: false,
                     role: 'bot',
                     delay: incrementor.set(3)
                 }
-            ])
-            await bot.users.update(userId, { meta: { subscribedToQuotes: 'disabled' }})
-        } else if (message.text == "KeepQuotes") {
+            ]);
+            await bot.users.update(userId, { meta: { subscribedToQuotes: 'disabled' } });
+        } else if (message.text === 'KeepQuotes') {
             await bot.send(conversation.id, [
                 {
-                    text: `Alright then, see you tomorrow.`,
+                    text: 'Alright then, see you tomorrow.',
                     isBackchannel: false,
                     role: 'bot',
                     delay: incrementor.set(3)
                 }
-            ])
-            await bot.users.update(userId, { meta: { subscribedToQuotes: 'true' }})
-        } else if (message.text == "StopQuotes") {
+            ]);
+            await bot.users.update(userId, { meta: { subscribedToQuotes: 'true' } });
+        } else if (message.text === 'StopQuotes') {
             await bot.send(conversation.id, [
                 {
-                    text: `Alright then. I won't bother you anymore.`,
+                    text: 'Alright then. I won\'t bother you anymore.',
                     isBackchannel: false,
                     role: 'bot',
                     delay: incrementor.set(3)
                 }
-            ])
-            await bot.users.update(userId, { meta: { subscribedToQuotes: 'disabled' }})
-        } else if (message.text == "StartQuotes") {
+            ]);
+            await bot.users.update(userId, { meta: { subscribedToQuotes: 'disabled' } });
+        } else if (message.text === 'StartQuotes') {
             await bot.send(conversation.id, [
                 {
-                    text: `Alright then, here is today's quote.`,
+                    text: 'Alright then, here is today\'s quote.',
                     isBackchannel: false,
                     role: 'bot',
                     delay: incrementor.set(3)
                 }
-            ])
-            await bot.users.update(userId, { meta: { subscribedToQuotes: 'true' }})
-            await depositQuote(userId)
-        } else if (message.text == "KeepQuotesOff") {
+            ]);
+            await bot.users.update(userId, { meta: { subscribedToQuotes: 'true' } });
+            await depositQuote(userId);
+        } else if (message.text === 'KeepQuotesOff') {
             await bot.send(conversation.id, [
                 {
-                    text: `Alright then, call me when you want to get daily quotes.`,
+                    text: 'Alright then, call me when you want to get daily quotes.',
                     isBackchannel: false,
                     role: 'bot',
                     delay: incrementor.set(3)
                 }
-            ])
-            await bot.users.update(userId, { meta: { subscribedToQuotes: 'disabled' }})
+            ]);
+            await bot.users.update(userId, { meta: { subscribedToQuotes: 'disabled' } });
         }
-    } catch (e) { errorCatch(e) }
+    } catch (e) { errorCatch(e); }
 });
 
 bot.on('message.create.*.command', (message, conversation) => {
-    log(message.text)
-    if (message.type === 'command' && message.text === "/assign") {
-        log("Hey")
-        const userId = message.user
+    log(message.text);
+    if (message.type === 'command' && message.text === '/assign') {
+        log('Hey');
+        const userId = message.user;
         bot.users.get(userId).then((user) => {
-            const userPreference = get(user, 'meta.subscribedToQuotes', 'disabled')
+            const userPreference = get(user, 'meta.subscribedToQuotes', 'disabled');
             if (userPreference === 'true') {
                 conversation.say([
                     {
-                        text: "Hey there. You are currently receiving daily quotes.",
+                        text: 'Hey there. You are currently receiving daily quotes.',
                         role: 'bot',
                         delay: incrementor.set(2)
                     },
                     {
-                        text: "Do you want to change this?",
+                        text: 'Do you want to change this?',
                         role: 'bot',
                         delay: incrementor.increment(3),
                         actions: [
                             {
-                                type: "reply",
-                                text: `Keep receiving quotes.`,
-                                payload: `KeepQuotes`
+                                type: 'reply',
+                                text: 'Keep receiving quotes.',
+                                payload: 'KeepQuotes'
                             },
                             {
-                                type: "reply",
-                                text: "Stop receiving quotes.",
-                                payload: `StopQuotes`
+                                type: 'reply',
+                                text: 'Stop receiving quotes.',
+                                payload: 'StopQuotes'
                             }
                         ]
                     }
-                ]).catch(errorCatch)
+                ]).catch(errorCatch);
             } else if (userPreference === 'disabled') {
                 conversation.say([
                     {
-                        text: "Hey there. You are currently not receiving daily quotes.",
+                        text: 'Hey there. You are currently not receiving daily quotes.',
                         role: 'bot',
                         delay: incrementor.set(2)
                     },
                     {
-                        text: "Do you want to receive quotes?",
+                        text: 'Do you want to receive quotes?',
                         role: 'bot',
                         delay: incrementor.increment(3),
                         actions: [
                             {
-                                type: "reply",
-                                text: `Start receiving quotes.`,
-                                payload: `StartQuotes`
+                                type: 'reply',
+                                text: 'Start receiving quotes.',
+                                payload: 'StartQuotes'
                             },
                             {
-                                type: "reply",
-                                text: "Keep not receiving quotes.",
-                                payload: `KeepQuotesOff`
+                                type: 'reply',
+                                text: 'Keep not receiving quotes.',
+                                payload: 'KeepQuotesOff'
                             }
                         ]
                     }
-                ]).catch(errorCatch)
+                ]).catch(errorCatch);
             }
         }).catch(errorCatch);
     }
-})
-
+});
 
 // Start Express.js webhook server to start listening
-bot.start()
+bot.start();
 /*
 const app = express()
 app.use("/bot", bot.router())
--- src/sheets.js
+++ src/sheets.js
@@ -1,47 +1,47 @@
 function getAuthenticatedClient() {
-  return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
     // create an oAuth client to authorize the API call.  Secrets are kept in a `keys.json` file,
     // which should be downloaded from the Google Developers Console.
-    const oAuth2Client = new OAuth2Client(
-      keys.web.client_id,
-      keys.web.client_secret,
-      keys.web.redirect_uris[0]
-      );
+        const oAuth2Client = new OAuth2Client(
+            keys.web.client_id,
+            keys.web.client_secret,
+            keys.web.redirect_uris[0]
+        );
 
-    // Generate the url that will be used for the consent dialog.
-    const authorizeUrl = oAuth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: 'https://www.googleapis.com/auth/drive.readonly, https://www.googleapis.com/auth/spreadsheets.readonly',
-  });
+        // Generate the url that will be used for the consent dialog.
+        const authorizeUrl = oAuth2Client.generateAuthUrl({
+            access_type: 'offline',
+            scope: 'https://www.googleapis.com/auth/drive.readonly, https://www.googleapis.com/auth/spreadsheets.readonly'
+        });
 
-    // Open an http server to accept the oauth callback. In this simple example, the
-    // only request to our webserver is to /oauth2callback?code=<code>
-    const server = http
-    .createServer(async (req, res) => {
-        try {
-            // acquire the code from the querystring, and close the web server.
-            const qs = new url.URL(req.url, 'http://localhost:4000').searchParams;
-            const code = qs.get('code');
-            console.log(`Code is ${code}`);guest
-            res.end('Authentication successful! Please return to the console.');
-            server.destroy();
+        // Open an http server to accept the oauth callback. In this simple example, the
+        // only request to our webserver is to /oauth2callback?code=<code>
+        const server = http
+            .createServer(async (req, res) => {
+                try {
+                    // acquire the code from the querystring, and close the web server.
+                    const qs = new url.URL(req.url, 'http://localhost:4000').searchParams;
+                    const code = qs.get('code');
+                    console.log(`Code is ${code}`); guest;
+                    res.end('Authentication successful! Please return to the console.');
+                    server.destroy();
 
-            // Now that we have the code, use that to acquire tokens.
-            const r = await oAuth2Client.getToken(code);
-            // Make sure to set the credentials on the OAuth2 client.
-            oAuth2Client.setCredentials(r.tokens);
-            console.info('Tokens acquired.', r.tokens);
-            resolve(oAuth2Client);
-        } catch (e) {
-          reject(e);
-      }
-  })
-    .listen(3000, () => {
-        // open the browser to the authorize url to start the workflow
-        open(authorizeUrl, {wait: false}).then(cp => cp.unref());
+                    // Now that we have the code, use that to acquire tokens.
+                    const r = await oAuth2Client.getToken(code);
+                    // Make sure to set the credentials on the OAuth2 client.
+                    oAuth2Client.setCredentials(r.tokens);
+                    console.info('Tokens acquired.', r.tokens);
+                    resolve(oAuth2Client);
+                } catch (e) {
+                    reject(e);
+                }
+            })
+            .listen(3000, () => {
+                // open the browser to the authorize url to start the workflow
+                open(authorizeUrl, { wait: false }).then((cp) => cp.unref());
+            });
+        destroyer(server);
     });
-    destroyer(server);
-});
 }
 
 const findQuote = async () => {
@@ -77,14 +77,13 @@ const findQuote = async () => {
         ['29-8-2020', "If at first you don't succeed. Try, try again.", 'William Edward Hickson'],
         ['30-8-2020', "Don't go through life, grow through life.", 'Eric Butterworth'],
         ['31-8-2020', 'Every novel is a mystery novel if you never finish it.', 'Unknown'],
-        ['1-9-2020', 'Every novel is a mystery novel if you never finish it.', 'Unknown'],
+        ['1-9-2020', 'Every novel is a mystery novel if you never finish it.', 'Unknown']
     ];
-    const date = new Date()
-    const dayDate = date.getDate()
-    const currentArray = answer[(dayDate - 1)]
-    const quote = currentArray[1]
-    return quote
+    const date = new Date();
+    const dayDate = date.getDate();
+    const currentArray = answer[(dayDate - 1)];
+    const quote = currentArray[1];
+    return quote;
 };
 
 module.exports = findQuote;
-
